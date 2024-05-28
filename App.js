fd9b66f056cdd7ef1145b7edeebbb9e12ae05915
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-05-27 23:59:06 -0400

    current app ui

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, ScrollView, Text, View, Linking } from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  const openMap = (lat, long) => {
+    const url = `https://www.google.com/maps/place/${lat},${long}`;
+    Linking.openURL(url);
+  };
+
+  useEffect(() => {
+    fetch('https://www.racooni.com/api/trash')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <View>
+          <ScrollView style={{ paddingLeft: 16, paddingRight: 16 }}>
+            <Text style={{ fontSize: 32, fontWeight: '700' }}>Racooni</Text>
+            <Text style={{ marginTop: 8, marginBottom: 16 }}>
+              Racooni is a place where you can find trash in Burlington on the side of the street. Simply tap on any of the items you see here and you'll be given directions to go pick it up
+            </Text>
+            {data?.map((trash) => (
+              <TouchableOpacity
+                onPress={() => openMap(trash.fields.lat, trash.fields.long)}
+                key={trash.fields.imgURL}
+              >
+                <View style={styles.imageContainer}>
+                  <Image
+                    source={{ uri: trash.fields.imgURL }}
+                    style={styles.image}
+                  />
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  image: {
+    aspectRatio: 3 / 4,
+    borderRadius: 16,
+    width: '100%',
   },
 });
